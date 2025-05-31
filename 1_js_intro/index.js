d3fc32e81fe7e97b20e64e64d2396de427bd5c79
yashvi-param <yashvirwn123@gmail.com>
2025-05-31 18:42:42 +0530

    Add files via upload
    complete

---
+++ 1_js_intro/index.js
@@ -0,0 +1,51 @@
+console.log("this is intro")
+
+console.log(10 + 20)
+
+let n = 20;
+let n1 = 20.5
+
+n1 = "float"
+
+console.log(n + n1);
+
+let arr = [1, "krisha", 2.5, true]
+
+console.log(arr)
+
+let obj = {
+    name:"amul milk",
+    price:30,
+    rate: 4.8
+}
+
+let obj2 = {};
+
+obj2.name = "yashwi"
+obj2.age = 20;
+obj2.marks = 80;
+
+
+console.log(obj)
+console.log(obj.price)
+
+
+
+document.write("<b>Name: </b>" + obj.name + "<br/>")
+document.write("<b>Price: </b>" + obj.price + "<br/>")
+document.write("<b>Rate: </b>" + obj.rate + "<br/>")
+
+// window.alert("thi sis ilart")
+
+console.table(arr)
+
+console.table(obj)
+
+// number
+// string
+// boolian
+// undefine
+// null
+
+// arr
+// object;
