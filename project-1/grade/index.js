b62fc3eb75ac870efaffdc614953d592e25b1733
yashvi-param <yashvirwn123@gmail.com>
2025-06-16 11:01:27 +0530

    Add files via upload
    1 complete

---
+++ project-1/grade/index.js
@@ -0,0 +1,28 @@
+let marks 
+
+marks1 = window.prompt("Enter Your Marks: ")
+marks2 = window.prompt("Enter Your Marks: ")
+
+
+if(marks > 75)
+{
+    document.write("your Grade is A")
+}
+else if(marks > 65)
+    {
+    document.write("your Grade is B")
+}
+else if(marks > 45)
+    {
+    document.write("your Grade is C")
+}
+else if(marks > 35)
+    {
+    document.write("your Grade is D")
+}
+else{
+    document.write("you failed..!")
+
+}
+
+
