0a5a97c1ae93b281d015468cd28c87f6da67a3c3
yashvi-param <yashvirwn123@gmail.com>
2025-08-25 16:58:40 +0530

    Add files via upload
    complete

---
+++ string/index.js
@@ -0,0 +1,30 @@
+
+
+
+let name = "abc";
+
+
+
+let text = "hello abc";
+
+console.log(text);
+
+
+console.log(text.charAt(6));
+
+
+
+console.log(text.concat("good morning"));
+
+
+console.log(text.endsWith("l"))
+
+console.log(text.includes("f"))
+
+console.log(text.indexOf("abc"));
+
+console.log(text.indexOf("def"))
+
+console.log(text.lastIndexOf("abc"));
+
+console.log(text.length);
