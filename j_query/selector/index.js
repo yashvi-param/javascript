474bdd7d93f65a912160c49d4c867e28d0a010a7
yashvi-param <yashvirwn123@gmail.com>
2025-09-09 18:29:21 +0530

    Add files via upload
    complete

---
+++ j_query/selector/index.js
@@ -0,0 +1,13 @@
+// $("*").css("background-color","gray")
+
+$("div>p").css("color", "blue")
+
+$("ul>li").css("color","red")
+
+$(".div").css("color", "green")
+
+$("[href]").css({
+    "color":"pink",
+    "text-decoration":"none",
+    "font-size":"30px",
+})
